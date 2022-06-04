b9c0e21f88c1de13a577147be04572ae99e33369
DivyaMaddipudi <30898620+DivyaMaddipudi@users.noreply.github.com>
2022-06-04 00:11:20 -0700

    Time: 2 ms (82.74%), Space: 57.5 MB (5.02%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,33 +1,40 @@
 class Solution {
     public int majorityElement(int[] nums) {
         
-        HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
-        
-        int majCount = 0;
+        int count = 0;
         int majority = 0;
-
+        
         for(int i=0;i<nums.length;i++) {
-            if(hm.containsKey(nums[i])) {
-                int count = hm.get(nums[i]);
+            if(count == 0) {
+                majority = nums[i];
+            }
+            
+            if(majority == nums[i]) {
                 count++;
-                hm.put(nums[i], count);
-            } else {
-                hm.put(nums[i], 1);
+            } else if(majority != nums[i]) {
+                count--;
             }
         }
         
-       for(Integer key: hm.keySet()) {
-           System.out.println(key + " : " + hm.get(key));
-           if(hm.get(key) > majCount) {
-               majCount = hm.get(key);
-               System.out.println(key + " key ");
-               majority = key;
-
-           }
-           // if(value >= nums.length/2) {
-           //     majCount = majCount + 1;
-           // }
-       }
+//         HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+//         int majCount = 0;
+//         int majority = 0;
+//         for(int i=0;i<nums.length;i++) {
+//             if(hm.containsKey(nums[i])) {
+//                 int count = hm.get(nums[i]);
+//                 count++;
+//                 hm.put(nums[i], count);
+//             } else {
+//                 hm.put(nums[i], 1);
+//             }
+//         }
+//        for(Integer key: hm.keySet()) {
+//            System.out.println(key + " : " + hm.get(key));
+//            if(hm.get(key) > majCount) {
+//                majCount = hm.get(key);
+//                majority = key;
+//            }
+//        }
         return majority;    
     }
 }
