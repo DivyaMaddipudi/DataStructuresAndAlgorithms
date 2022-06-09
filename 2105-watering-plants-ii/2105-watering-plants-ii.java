88c14d213a6392a07ee80c400b9caf5cfaba735c
DivyaMaddipudi <30898620+DivyaMaddipudi@users.noreply.github.com>
2022-06-08 18:38:18 -0700

    Time: 1900 ms (6.49%), Space: 124 MB (5.41%) - LeetHub

--- 2105-watering-plants-ii/2105-watering-plants-ii.java
+++ 2105-watering-plants-ii/2105-watering-plants-ii.java
@@ -1,19 +1,16 @@
 class Solution {
     public int minimumRefill(int[] plants, int capacityA, int capacityB) {
-        
         int A=0;
         int B=plants.length-1;
         int refill = 0;
         int mid = (A+B)/2;
         int initialCapA = capacityA;
         int initialCapB = capacityB;
-        
         while(A<=B) {
             System.out.println("in while");
             if(A!=B) {
             if(plants[A] > capacityA && plants[B] > capacityB) {
             System.out.println("refill");
-                                
                 refill = refill+2;
                 capacityA = initialCapA;
                 capacityB = initialCapB;
@@ -42,8 +39,6 @@ class Solution {
                 B--;
             }
             } else {
-            System.out.println(" A==B else");
-                
             if(plants[A] > capacityA && plants[B] > capacityB) {
                refill++;
            } else if(plants[A] < capacityA) {
@@ -56,19 +51,6 @@ class Solution {
             break;
          }
         }
-        
-        
-        // if(plants.length%2 !=0 ) {
-        //    if(plants[mid] > capacityA && plants[mid] > capacityB) {
-        //        refill++;
-        //    } else if(plants[mid] < capacityA) {
-        //        capacityA -= plants[A];
-        //    } else if(plants[mid] < capacityB) {
-        //        capacityB -= plants[B];
-        //    } else {
-        //        capacityA -= plants[A];
-        //    }
-        // }
         return refill;
     }
 }
