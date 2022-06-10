addf218560dc4a85a240dfff5765c87aefa23ae5
DivyaMaddipudi <30898620+DivyaMaddipudi@users.noreply.github.com>
2022-06-10 11:40:47 -0700

    Time: 333 ms (5.44%), Space: 53.4 MB (70.96%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -1,17 +1,46 @@
 class Solution {
     public void reverseString(char[] s) {
         
-        int l=0;
-        int r = s.length-1;
-        while(l<r) {
-            char temp = s[l];
-            s[l] = s[r];
-            s[r] = temp;
-            l++;
-            r--;
+        
+        String str = printRev(s);
+        char[] st = str.toCharArray();
+        for(int i=0;i<s.length;i++) {
+            s[i] = st[i];
         }
-       for(int i=0;i<s.length;i++) {
-            System.out.println(s[i]);
-       }
+        System.out.println(s);
+        
+        // TWO POINTER
+       //  int l=0;
+       //  int r = s.length-1;
+       //  while(l<r) {
+       //      char temp = s[l];
+       //      s[l] = s[r];
+       //      s[r] = temp;
+       //      l++;
+       //      r--;
+       //  }
+       // for(int i=0;i<s.length;i++) {
+       //      System.out.println(s[i]);
+       // }
+    }
+    
+    public String printRev(char[] s) {
+        if(s.length == 0) {
+            return "";       
+        }
+        
+        if(s.length == 1) {
+            return new String(s);
+        }
+        
+        //RECURSION
+        char temp = s[s.length - 1];
+         char first = s[s.length-1];
+         char last = s[0];
+        System.out.println(s.length);
+        char[] arr2 = Arrays.copyOfRange(s,1,s.length-1);
+       
+        // String str = first + last;
+        return first + printRev(arr2) + last;   
     }
 }
