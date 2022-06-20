98bad18b88a182557b5202494e5d8c7df678170a
DivyaMaddipudi <30898620+DivyaMaddipudi@users.noreply.github.com>
2022-06-20 13:20:55 -0700

    Time: 848 ms (5.49%), Space: 118.3 MB (5.48%) - LeetHub

---
+++ 820-short-encoding-of-words/820-short-encoding-of-words.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int minimumLengthEncoding(String[] words) {
+        
+        String ans = "";
+        String max = "";
+        
+        for(int i=0;i<words.length-1;i++) {
+            for(int j=i+1;j<words.length;j++) {
+                if(words[i].length() < words[j].length() ) {
+                    String temp = words[i];
+                    words[i] = words[j];
+                    words[j] = temp;
+                }
+            }
+        }
+        for(int i=0;i<words.length;i++) {
+                System.out.println(words[i]);
+            
+            if(words[i].length() == 1) {
+                int index = ans.lastIndexOf(words[i]);
+                int length = index + words[i].length();
+                System.out.println(index + " -------- " + length);
+                if(ans == "") {
+                    ans += words[i] + "#";
+                } else if(ans.charAt(length) != '#') {
+                    ans += words[i]+"#";
+                }
+            } else {
+            
+            if(ans.contains(words[i])) {
+                int index = ans.indexOf(words[i]);
+                int length = index + words[i].length();
+                System.out.println(index + " -------- " + length);
+                if(ans.charAt(length) != '#') {
+                    ans += words[i]+"#";
+                }
+                
+            } else {
+                 ans += words[i]+"#";
+            }
+            }
+        }
+        System.out.println(ans);
+        return ans.length();
+    }
+}
