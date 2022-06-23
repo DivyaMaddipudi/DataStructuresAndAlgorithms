8b16e34aff31e47182f4998428e56d8148bd3465
DivyaMaddipudi <30898620+DivyaMaddipudi@users.noreply.github.com>
2022-06-22 21:43:40 -0700

    Time: 12 ms (34.25%), Space: 42.7 MB (51.51%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,25 +1,40 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         
-        int j = 0;
-        int newRow = 0;
-        for(int row=0;row<matrix.length-1;row++) {
-            System.out.println(matrix[row].length);
-            
-           if(matrix[row][j] <= target) {
-               if(matrix[row+1][j] > target) {
-                    newRow = row;
-               } else if (matrix[row+1][j] <= target) {
-                   newRow = row+1;
-               }
-            } 
-        }
+        int i = 0; 
+        int j = matrix[0].length-1;
         
-        for(int col = 0;col<matrix[newRow].length;col++) {
-            if(matrix[newRow][col] == target) {
+        while(i<matrix.length && j>=0) {
+            System.out.println(matrix[i][j] + " --- " + i + " " + j);
+            if(matrix[i][j] == target) {
                 return true;
+            } else if(matrix[i][j] < target) {
+                i++;
+            } else if(matrix[i][j] > target) {
+                j--;
             }
         }
         return false;
+        
+//         int j = 0;
+//         int newRow = 0;
+//         for(int row=0;row<matrix.length-1;row++) {
+//             System.out.println(matrix[row].length);
+            
+//            if(matrix[row][j] <= target) {
+//                if(matrix[row+1][j] > target) {
+//                     newRow = row;
+//                } else if (matrix[row+1][j] <= target) {
+//                    newRow = row+1;
+//                }
+//             } 
+//         }
+        
+//         for(int col = 0;col<matrix[newRow].length;col++) {
+//             if(matrix[newRow][col] == target) {
+//                 return true;
+//             }
+//         }
+//         return false;
     }
 }
