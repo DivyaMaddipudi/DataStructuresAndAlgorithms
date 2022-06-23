9cb534b5bf43041a5ed2bf7659db9daef7f988bf
DivyaMaddipudi <30898620+DivyaMaddipudi@users.noreply.github.com>
2022-06-22 21:30:19 -0700

    Time: 6 ms (34.25%), Space: 43.2 MB (17.35%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,12 +1,6 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         
-        System.out.println(matrix.length + " row ");
-        // System.out.println(matrix[matrix.length].length + " col ");
-        // if(matrx.length)
-        
-        // if(matrix[0][0] == target) return matrix[0][0] == target;
-        
         int j = 0;
         int newRow = 0;
         for(int row=0;row<matrix.length-1;row++) {
@@ -18,18 +12,10 @@ class Solution {
                } else if (matrix[row+1][j] <= target) {
                    newRow = row+1;
                }
-               // System.out.println(matrix[row+1][j] + " " + target + " T/F");
             } 
         }
         
-        System.out.println(matrix[newRow].length + " row " + newRow);
-        
-        // if(matrix[newRow].length == 0) {
-        //     return matrix[newRow][0] == target;
-        // }
         for(int col = 0;col<matrix[newRow].length;col++) {
-            // System.out.println(matrix[newRow][col] + " ===== ");
-            
             if(matrix[newRow][col] == target) {
                 return true;
             }
