75f1857debe3b2657b5ddae1cd3c98f7103dea46
DivyaMaddipudi <30898620+DivyaMaddipudi@users.noreply.github.com>
2022-06-25 19:10:38 -0700

    Time: 0 ms (100.00%), Space: 42.3 MB (84.20%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.java
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.java
@@ -13,16 +13,11 @@ class Solution {
         int low = 0;
         int high = mountainArr.length()-1;
         int mid = findPeak(mountainArr);
-        System.out.println(mid + " mid " + mountainArr.get(mid));
-        
         int indexFirst = binarySearch(target, mountainArr, low, mid);
         if(indexFirst != -1) {
-            System.out.println(" Hello ");
             return indexFirst;
         } else{
             int indexLast = binarySearchDesc(target, mountainArr, mid+1, high);
-            System.out.println(" Hello in desc ");
-            
             return indexLast;
         }
     }
@@ -68,13 +63,9 @@ class Solution {
      private int binarySearchDesc(int target, MountainArray mountainArr, int low, int high) {
         
         int val = low + (high - low )/2;
-        System.out.println(val + " mid in desc sort" + mountainArr.get(val));
-         
          
         while(low <= high) {
             int mid = low + (high - low )/2;
-            
-        System.out.println(low + " lh " + high);
             
             if(mountainArr.get(mid) == target) {
                 return mid;
