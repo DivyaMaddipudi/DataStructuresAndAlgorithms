bcb052c9ae9ccad5aaab88e4fb58a57919986b69
DivyaMaddipudi <30898620+DivyaMaddipudi@users.noreply.github.com>
2022-06-28 19:34:04 -0700

    Time: 15 ms (7.12%), Space: 41 MB (86.77%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.java
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -5,31 +5,23 @@ class Solution {
         int count = 0;
         for(int i=0;i<s.length();i++) {
             if(hm.containsKey(s.charAt(i))) {
-                System.out.println(" hello ");
                 int value = hm.get(s.charAt(i));
                 value++;
                 hm.put(s.charAt(i), value);
             } else {
-                System.out.println(" checking else ");
-                
                 hm.put(s.charAt(i), 1);
             } 
         }
-                System.out.println(hm);
-        
-        
         for(int j=0;j<s.length();j++) {
-            
-                if(hm.containsKey(s.charAt(j))) {
-                    if(hm.get(s.charAt(j))%2 == 0) {
-                        count+= hm.get(s.charAt(j));
-                        hm.remove(s.charAt(j));
-                    } else {
-                        count+= hm.get(s.charAt(j))-1;
-                        hm.put(s.charAt(j), 1);
-                    }
+            if(hm.containsKey(s.charAt(j))) {
+                if(hm.get(s.charAt(j))%2 == 0) {
+                    count+= hm.get(s.charAt(j));
+                    hm.remove(s.charAt(j));
+                } else {
+                    count+= hm.get(s.charAt(j))-1;
+                    hm.put(s.charAt(j), 1);
                 }
-            
+            }
         }
         
         if(hm.size() != 0) {
