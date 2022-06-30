7da17401078c635f1ae27e14e8ce7a4ed51e68b0
DivyaMaddipudi <30898620+DivyaMaddipudi@users.noreply.github.com>
2022-06-29 23:21:31 -0700

    Time: 1262 ms (7.53%), Space: 104.3 MB (5.64%) - LeetHub

--- 713-subarray-product-less-than-k/713-subarray-product-less-than-k.java
+++ 713-subarray-product-less-than-k/713-subarray-product-less-than-k.java
@@ -1,19 +1,38 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
+        //Sliding window technique
+        
+        if(k<= 0) return 0;
+        int s = 0;
         int prod = 1;
-        int start = 0;
-        int count = 0;
-        for(int end=0;end<nums.length;end++) {
-            prod = 1;
-            for(int i=end;i<nums.length;i++) {
-                prod *= nums[i];
-                if(prod < k) {
-                 count++;   
-                } else if(prod >= k) {
-                    break;
-                }
+        int res = 0;
+        for(int i=0;i<nums.length;i++) {
+            prod *= nums[i];
+            while(prod>=k && s<=i) {
+                prod = prod/nums[s];
+                s++;
             }
+            res += i - s + 1;
+            System.out.println(res);
+            
         }
-        return count;
+        return res;
+        //BRUTE FORCE
+        
+        // int prod = 1;
+        // int start = 0;
+        // int count = 0;
+        // for(int end=0;end<nums.length;end++) {
+        //     prod = 1;
+        //     for(int i=end;i<nums.length;i++) {
+        //         prod *= nums[i];
+        //         if(prod < k) {
+        //          count++;   
+        //         } else if(prod >= k) {
+        //             break;
+        //         }
+        //     }
+        // }
+        // return count;
     }
 }
