096686d09c3dd67264c1a2fb8072dbadd2dd5813
DivyaMaddipudi <30898620+DivyaMaddipudi@users.noreply.github.com>
2022-07-03 08:01:33 -0700

    Time: 3 ms (90.32%), Space: 45.5 MB (44.64%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,14 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-       
+     
         HashMap<Integer, Integer> hm = new HashMap<>();
         for(int i=0;i<nums.length;i++) {
-            int complement = target - nums[i];
-            if(hm.containsKey(complement)) {
-                return new int[] {hm.get(complement), i};
+            int diff = target - nums[i];
+            if(hm.containsKey(diff)) {
+                return new int[] {hm.get(diff), i};
+            } else {
+                hm.put(nums[i], i);
             }
-            hm.put(nums[i], i);
         }
-        throw new IllegalArgumentException("No match found");
+     return new int[] {};
     }
 }
