9506159b44c3aa6055d8d8a2eeea98df170094d1
DivyaMaddipudi <30898620+DivyaMaddipudi@users.noreply.github.com>
2022-07-06 16:33:33 -0700

    Time: 185 ms (5.06%), Space: 69.7 MB (5.07%) - LeetHub

---
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int numberOfBeams(String[] bank) {
+        
+        int m = bank.length;
+        int n = bank[0].length();
+       
+        int[][] matrix = new int[m][n];
+        
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                matrix[i][j] = Integer.valueOf(bank[i].charAt(j)-'0');
+            }
+        }
+        
+        
+        int count = 0;
+        int prev = 0;
+        for(int i=0;i<m;i++) {
+            int rowOne = 0;
+            for(int j=0;j<n;j++) {
+                if(matrix[i][j] == 1) {
+                    // count++;
+                    rowOne++;
+                    
+                }
+            }
+            
+            System.out.println(prev +  " ++++++++++++++ " + rowOne + " +++++++++++++++ " + count );
+            
+            count = count + (prev * rowOne);
+            // prev = rowOne;
+            if(rowOne != 0) {
+                prev=rowOne;
+            }
+            
+            System.out.println(prev + " ==== " + rowOne + " ===== " + count );
+        }
+        
+            System.out.println(count);
+        
+        return count;
+        
+    }
+}
