5270166bf5a2662979e38a93f6d2c53b6225f912
DivyaMaddipudi <30898620+DivyaMaddipudi@users.noreply.github.com>
2022-07-06 17:06:06 -0700

    Time: 78 ms (20.47%), Space: 117.8 MB (5.21%) - LeetHub

--- 575-distribute-candies/575-distribute-candies.java
+++ 575-distribute-candies/575-distribute-candies.java
@@ -3,15 +3,34 @@ class Solution {
         
         int n = candyType.length;
         int canEat = n/2;
-        Arrays.sort(candyType);
-        int count = 1;
-        for(int i=1;i<n;i++) {
-           if(count < canEat) {
-                if(candyType[i] != candyType[i-1]) {
-                    count++;
-                }
+        
+        HashSet<Integer> set = new HashSet<>();
+         for(int i=0;i<n;i++) {
+           if(!set.contains(candyType[i])) {
+               set.add(candyType[i]);
            }
         }
-        return count;
+        
+        if(canEat <= set.size()) {
+            return canEat;
+        } else {
+            return set.size();
+        }
+        
+        //TC: O(n log(n)) - for sort function 
+        //SC: O(1)
+        
+        // int n = candyType.length;
+        // int canEat = n/2;
+        // Arrays.sort(candyType);
+        // int count = 1;
+        // for(int i=1;i<n;i++) {
+        //    if(count < canEat) {
+        //         if(candyType[i] != candyType[i-1]) {
+        //             count++;
+        //         }
+        //    }
+        // }
+        // return count;
     }
 }
