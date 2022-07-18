a734c998fa244b262c7eed8aa0e2d4a1cd68296b
DivyaMaddipudi <30898620+DivyaMaddipudi@users.noreply.github.com>
2022-07-18 10:21:20 -0700

    Time: 103 ms (8.74%), Space: 47.7 MB (5.26%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
@@ -14,13 +14,33 @@
  * }
  */
 class Solution {
+    int max = Integer.MIN_VALUE;
     public int maxDepth(TreeNode root) {
         
         if(root == null) return 0;
         
-        int l = maxDepth(root.left);
-        int r = maxDepth(root.right);
+//         int l = maxDepth(root.left);
+//         int r = maxDepth(root.right);
         
-        return 1+Math.max(l, r);
+//         return 1+Math.max(l, r);
+        helper(root, 0);
+        return max+1;
+    }
+    
+    private void helper(TreeNode root, int count) {
+        // System.out.println(count + " count ");
+        if(root == null) {
+            max = Math.max(max, count);
+            return;
+        }
+        System.out.println(count + " pre " + root.val);
+        if(root.left != null || root.right != null) {
+            count++;
+        }
+        helper(root.left, count);
+        System.out.println(count + " in " + root.val);
+        helper(root.right, count);
+        System.out.println(count + " post " + root.val);
+        
     }
 }
