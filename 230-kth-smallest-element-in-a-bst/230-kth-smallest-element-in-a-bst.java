145f491cbaac3b16a6dd9019cfa1eb8f87fe8e7b
DivyaMaddipudi <30898620+DivyaMaddipudi@users.noreply.github.com>
2022-07-18 18:00:08 -0700

    Time: 20 ms (5.57%), Space: 46 MB (21.58%) - LeetHub

---
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int size;
+    int value;
+    public int kthSmallest(TreeNode root, int k) {
+      
+        size = k;
+        helper(root);
+        return value;
+        
+    }
+    private void helper(TreeNode root) {
+       
+        if(size < 0) {
+            return;
+        }
+        if(root == null) {
+            return;
+        }
+
+        
+        helper(root.left);
+        System.out.println(root.val + " in " + size);
+        
+        size--;
+        if(size == 0) {
+            value = root.val;
+            return;
+        }
+      
+        helper(root.right);
+        // System.out.println(root.val + " post " + size);
+        
+        // if(size == 0) {
+        //     value = root.val;
+        //     return;
+        // }
+        
+    }
+}
