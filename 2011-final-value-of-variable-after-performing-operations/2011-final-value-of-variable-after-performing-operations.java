dc4289da32d979dd3af91939afcc8fa5d1bd7f7d
DivyaMaddipudi <30898620+DivyaMaddipudi@users.noreply.github.com>
2022-07-19 17:35:18 -0700

    Time: 1 ms (96.58%), Space: 43.1 MB (53.51%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
@@ -2,7 +2,7 @@ class Solution {
     public int finalValueAfterOperations(String[] operations) {
         int value = 0;
         for(int i=0;i<operations.length;i++) {
-            if(operations[i].contains('+' + "")) {
+            if(operations[i].charAt(1) == '+') {
                 value++;
             } else {
                 value--;
