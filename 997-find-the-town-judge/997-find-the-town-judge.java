52ff17ecb023e767885227caf5eab5287c3e8750
DivyaMaddipudi <30898620+DivyaMaddipudi@users.noreply.github.com>
2022-07-21 18:32:44 -0700

    Time: 2 ms (99.78%), Space: 74.1 MB (30.41%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.java
+++ 997-find-the-town-judge/997-find-the-town-judge.java
@@ -1,43 +1,53 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
         
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
-        
-        for(int i=0;i<=n;i++) {
-            adj.add(new ArrayList<>());
-        }
-        
+        int[] count = new int[n+1];
         for(int i=0;i<trust.length;i++) {
-            adj.get(trust[i][0]).add(trust[i][1]);
-        }
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        for(int i=1;i<=n;i++) {
-            hm.put(i, 0);
-        }
-        for(List<Integer> list : adj) {
-                System.out.println(list);
-            
-            for(int i=0;i<list.size();i++) {
-                System.out.println(list.get(i));
-                int count = hm.get(list.get(i));
-                count++;
-                hm.put(list.get(i), count);
-            }
-        }
-            System.out.println(hm);
-        
-        int idx = 0;
-        for(int i=1;i<=n;i++) {
-            if(hm.get(i) == n -1) {
-                idx = i;
-                if(adj.get(idx).size() == 0) {
-                    return idx;
-                 }
-            }
+            count[trust[i][0]]--;
+            count[trust[i][1]]++;
         }
         
-    
-       
+        for(int i=1;i<=n;i++) {
+            if(count[i] == n-1) {
+                return i;
+            }
+        }
         return -1;
+        
+//         ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+        
+//         for(int i=0;i<=n;i++) {
+//             adj.add(new ArrayList<>());
+//         }
+        
+//         for(int i=0;i<trust.length;i++) {
+//             adj.get(trust[i][0]).add(trust[i][1]);
+//         }
+//         HashMap<Integer, Integer> hm = new HashMap<>();
+//         for(int i=1;i<=n;i++) {
+//             hm.put(i, 0);
+//         }
+//         for(List<Integer> list : adj) {
+//                 System.out.println(list);
+            
+//             for(int i=0;i<list.size();i++) {
+//                 System.out.println(list.get(i));
+//                 int count = hm.get(list.get(i));
+//                 count++;
+//                 hm.put(list.get(i), count);
+//             }
+//         }
+//             System.out.println(hm);
+        
+//         int idx = 0;
+//         for(int i=1;i<=n;i++) {
+//             if(hm.get(i) == n -1) {
+//                 idx = i;
+//                 if(adj.get(idx).size() == 0) {
+//                     return idx;
+//                  }
+//             }
+//         }
+//         return -1;
     }
 }
