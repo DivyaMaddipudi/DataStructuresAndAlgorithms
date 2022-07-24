f82d5680ab750004c9eca8abfe325bb03fee4de8
DivyaMaddipudi <30898620+DivyaMaddipudi@users.noreply.github.com>
2022-07-24 13:32:57 -0700

    Time: 3 ms (50.92%), Space: 42.7 MB (85.31%) - LeetHub

--- 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.java
+++ 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.java
@@ -14,40 +14,52 @@
  * }
  */
 class Solution {
+    ArrayList<Integer> ans;
     public List<Integer> largestValues(TreeNode root) {
+    
+        // DFS
+        ans = new ArrayList<>();
+        // ans.add(-1);
+        dfs(root, 0);
+        System.out.println(ans);
         
-        Queue<TreeNode> queue = new LinkedList<>();
-        ArrayList<Integer> res = new ArrayList<>();
-        if(root == null ) return res;
+        return ans;
         
-        queue.add(root);
-        // res.add(root.val);
-        while(!queue.isEmpty()) {
-            int size = queue.size();
-            int Max = Integer.MIN_VALUE;
-            for(int i=0;i<size;i++) {
-                 TreeNode curr = queue.poll();
-                 Max = Math.max(Max,curr.val);
-                if(curr.left != null) {
-                    queue.add(curr.left);
-                }
-                if(curr.right != null) {
-                    queue.add(curr.right);
-                }
-            }
-            res.add(Max);
+        
+        // BFS
+        // Queue<TreeNode> queue = new LinkedList<>();
+        // ArrayList<Integer> res = new ArrayList<>();
+        // if(root == null ) return res;
+        // queue.add(root);
+        // while(!queue.isEmpty()) {
+        //     int size = queue.size();
+        //     int Max = Integer.MIN_VALUE;
+        //     for(int i=0;i<size;i++) {
+        //          TreeNode curr = queue.poll();
+        //          Max = Math.max(Max,curr.val);
+        //         if(curr.left != null) {
+        //             queue.add(curr.left);
+        //         }
+        //         if(curr.right != null) {
+        //             queue.add(curr.right);
+        //         }
+        //     }
+        //     res.add(Max);
+        // }
+        // return res;
+        
+    }
+    
+    private void dfs(TreeNode root, int lvl) {
+        if(root == null) return;
+        
+        if(lvl == ans.size()) {
+            ans.add(root.val);
+        } else {
+            ans.set(lvl, Math.max(ans.get(lvl), root.val));
         }
-        return res;
+        
+        dfs(root.left, lvl+1);
+        dfs(root.right, lvl+1);
     }
 }
-
-
-//  
-//             
-
-            
-//             if(curr.right != null) {
-//                 Max = Math.max(Max, curr.right.val);
-//                 queue.add(curr.right);
-//             }
-//             res.add(Max);
