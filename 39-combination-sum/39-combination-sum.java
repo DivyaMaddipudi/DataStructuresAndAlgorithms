0d0985f0ce1cd39533b4f39572ae7847959cc3db
DivyaMaddipudi <30898620+DivyaMaddipudi@users.noreply.github.com>
2022-07-25 16:47:14 -0700

    Time: 17 ms (10.74%), Space: 45.5 MB (37.51%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,23 +1,26 @@
 class Solution {
+    int val;
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         
         List<List<Integer>> ans = new ArrayList<List<Integer>>();
         List<Integer> al = new ArrayList<>();
+        val = target;
         int size = candidates.length;
-        combinations(0, candidates, target, ans, al, size);
+        combinations(0, candidates, 0, ans, al, size);
         return ans;
     }
     
     public void combinations(int index, int[] cands, int target,  List<List<Integer>>  ans, List<Integer> al, int size) {
+        
         if(index == size) {
-            if(target == 0) {
+            if(target == val) {
                  ans.add(new ArrayList<>(al));
             }
             return;
         }
-        if(cands[index] <= target) {
+        if(target < val) {
             al.add(cands[index]);
-            combinations(index, cands, target-cands[index], ans, al, size);
+            combinations(index, cands, target+cands[index], ans, al, size);
             al.remove(Integer.valueOf(cands[index]));
         }
         combinations(index+1, cands, target, ans, al, size);
