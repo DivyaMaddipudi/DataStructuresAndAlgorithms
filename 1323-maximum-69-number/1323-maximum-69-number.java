4f5daa625ec69b68b625fdfdd1e4e13fdc5e926e
DivyaMaddipudi <30898620+DivyaMaddipudi@users.noreply.github.com>
2022-07-25 11:12:46 -0700

    Time: 11 ms (9.97%), Space: 41.1 MB (41.89%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.java
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -1,31 +1,36 @@
 class Solution {
     public int maximum69Number (int num) {
-        int digitsCount = (int) Math.log10(num) + 1;
-        int[] numsArr = new int[digitsCount];
-        String str = String.valueOf(num);
-        for(int i=0;i<digitsCount;i++) {
-           numsArr[i] = (int) str.charAt(i)-'0';
-        }
-        int max = num;
-        for(int i=0;i<digitsCount;i++) {
-            int temp = numsArr[i];
-            System.out.println(Arrays.toString(numsArr));
+        
+        //OPTIMIZED
+        
+        return Integer.parseInt(String.valueOf(num).replaceFirst("6", "9"));        
+        //BRUTE FORCE
+//         int digitsCount = (int) Math.log10(num) + 1;
+//         int[] numsArr = new int[digitsCount];
+//         String str = String.valueOf(num);
+//         for(int i=0;i<digitsCount;i++) {
+//            numsArr[i] = (int) str.charAt(i)-'0';
+//         }
+//         int max = num;
+//         for(int i=0;i<digitsCount;i++) {
+//             int temp = numsArr[i];
+//             System.out.println(Arrays.toString(numsArr));
             
-            if(numsArr[i] == 6) {
-                numsArr[i] = 9;
+//             if(numsArr[i] == 6) {
+//                 numsArr[i] = 9;
                
-            } else if(numsArr[i] == 9) {
-                numsArr[i] = 6;
+//             } else if(numsArr[i] == 9) {
+//                 numsArr[i] = 6;
                 
-            }
-            int value = 0;
-            for(int j=0;j<digitsCount;j++) {
-                value = (int)  (value * Math.pow(10, 1))+ numsArr[j];
-            }
-            max = Math.max(max, value);
-            numsArr[i] = temp;
-        }
-        return max;
+//             }
+//             int value = 0;
+//             for(int j=0;j<digitsCount;j++) {
+//                 value = (int)  (value * Math.pow(10, 1))+ numsArr[j];
+//             }
+//             max = Math.max(max, value);
+//             numsArr[i] = temp;
+//         }
+//         return max;
         
     }
 }
