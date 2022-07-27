cd0e66f7b362e3ef82893fb4917478c97ba3a747
DivyaMaddipudi <30898620+DivyaMaddipudi@users.noreply.github.com>
2022-07-27 16:54:34 -0700

    Time: 1331 ms (5.05%), Space: 47 MB (6.63%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -1,20 +1,48 @@
 class Solution {
     public int minMoves2(int[] nums) {
-        
         long min = Long.MAX_VALUE;
-        System.out.println(min);
-        for(int j=0;j<nums.length;j++) {
-            long value = nums[j];
-            long steps = 0;
-            for(int i=0;i<nums.length;i++) {
-                if(nums[i] <= value) {
-                   steps += value - nums[i];
-               } else if(nums[i] > value) {
-                    steps += nums[i] - value;
-                }
+        
+        // OPTIMIZED
+        int ptr = 0;
+        int i = 0;
+        long steps = 0;
+        
+        while(ptr < nums.length) {
+            long value = nums[ptr];
+            // System.out.println(ptr + " " + i + " steps " + steps);
+            if(nums[i] <= value) {
+               steps += value - nums[i];
+           } else if(nums[i] > value) {
+                steps += nums[i] - value;
             }
-            min = Math.min(min, steps);
+            i++;
+            
+            if(i == nums.length) {
+                min = Math.min(min, steps);
+                ptr++;
+                i=0;
+                steps = 0;
+            }
+          
         }
         return (int)min;
+        
+        
+        // BRUTE FORCE
+        // O(N**2)
+        
+        // for(int j=0;j<nums.length;j++) {
+        //     long value = nums[j];
+        //     long steps = 0;
+        //     for(int i=0;i<nums.length;i++) {
+        //         if(nums[i] <= value) {
+        //            steps += value - nums[i];
+        //        } else if(nums[i] > value) {
+        //             steps += nums[i] - value;
+        //         }
+        //     }
+        //     min = Math.min(min, steps);
+        // }
+        // return (int)min;
     }
 }
