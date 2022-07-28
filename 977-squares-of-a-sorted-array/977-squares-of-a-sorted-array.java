a37f67d92dfd264dadbff41506d1e27efee2ca86
DivyaMaddipudi <30898620+DivyaMaddipudi@users.noreply.github.com>
2022-07-27 18:47:51 -0700

    Time: 45 ms (8.18%), Space: 58.5 MB (5.08%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
@@ -1,22 +1,33 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
        
-        int[] output = new int[nums.length];
-       
-        int outputPtr = nums.length - 1;
-        int low = 0;
-        int high = nums.length - 1;
-        while(low <= high) {
-            if(Math.abs(nums[low]) < Math.abs(nums[high])) {
-                output[outputPtr] = (int) Math.pow(nums[high],2);
-                high--;
-            } else {
-                output[outputPtr] = (int) Math.pow(nums[low],2);
-                low++;
-            }
-            outputPtr--;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> a - b);
+        for(int i=0;i<nums.length;i++) {
+            nums[i] = (int) Math.pow(nums[i], 2);
         }
+        for(int i=0;i<nums.length;i++) {
+            pq.add(nums[i]);   
+        }
+        
+        for(int i=0;i<nums.length;i++) {
+            nums[i] = pq.poll();
+        }
+        
+        // int[] output = new int[nums.length];
+        // int outputPtr = nums.length - 1;
+        // int low = 0;
+        // int high = nums.length - 1;
+        // while(low <= high) {
+        //     if(Math.abs(nums[low]) < Math.abs(nums[high])) {
+        //         output[outputPtr] = (int) Math.pow(nums[high],2);
+        //         high--;
+        //     } else {
+        //         output[outputPtr] = (int) Math.pow(nums[low],2);
+        //         low++;
+        //     }
+        //     outputPtr--;
+        // }
             
-        return output;
+        return nums;
     }
 }
