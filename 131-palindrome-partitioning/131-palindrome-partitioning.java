bff7f5b415ff61bb4458cf3c4953218887e3ef01
DivyaMaddipudi <30898620+DivyaMaddipudi@users.noreply.github.com>
2022-07-28 13:17:18 -0700

    Time: 374 ms (5.12%), Space: 150.5 MB (66.03%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -0,0 +1,49 @@
+class Solution {
+    
+    ArrayList<List<String>> result;
+    public List<List<String>> partition(String s) {
+        result = new ArrayList<List<String>>();
+        if(s.length() == 0 || s == null) return result;
+        helper(0, new ArrayList<>(), s);
+        
+        return result;
+    }
+    
+    private void helper(int index, ArrayList<String> path, String s) {
+        //base case
+        if(index == s.length()) {
+            System.out.println(path + " in base cdn ");
+            for(int i=0;i<path.size();i++) {
+                if(!isPalindrome(path.get(i))) {
+                    return;
+                }
+            }
+            result.add(path);
+            return;
+        }
+        // logic 
+        for(int i=index;i<s.length();i++) {
+            String str = s.substring(index, i+1);
+            ArrayList<String> newlist = new ArrayList<>(path);
+            newlist.add(str);
+            // path.add(str);
+            // System.out.println(path);
+            helper(i+1, newlist, s);
+        }
+    }
+    
+    private boolean isPalindrome(String s) {
+        if(s.length() == 1) return true;
+        int low = 0;
+        int high = s.length()-1;
+        while(low <= high) {
+            if(s.charAt(low) == s.charAt(high)) {
+                low++;
+                high--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
