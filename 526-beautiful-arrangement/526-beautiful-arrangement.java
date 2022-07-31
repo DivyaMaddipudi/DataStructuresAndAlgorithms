39aef845584256fece9b04a9a08f94c22fec55d7
DivyaMaddipudi <30898620+DivyaMaddipudi@users.noreply.github.com>
2022-07-31 14:07:42 -0700

    Time: 1384 ms (5.01%), Space: 72.1 MB (5.43%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.java
+++ 526-beautiful-arrangement/526-beautiful-arrangement.java
@@ -11,17 +11,14 @@ class Solution {
         if(al.size() == n) {
             System.out.println(al);
             count++;
-            // System.out.println(count + " count ");
             return;
         }
         
         
         for(int i=1;i<=n;i++) {
             if(!al.contains(i)) {
-                // System.out.println((al.size()+1)%i + "  " + i%(al.size()+1));
                 if((al.size()+1)%i == 0 || i%(al.size()+1) == 0) {
                      al.add(i);
-                    // System.out.println( i + " " + " after adding " + al.size() + " al size " + al);
                     helper( index+1, n, al);
                     al.remove(al.size()-1);
                 }
