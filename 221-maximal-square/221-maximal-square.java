bbfb76876ef275750d21028926243d6f674e1b84
DivyaMaddipudi <30898620+DivyaMaddipudi@users.noreply.github.com>
2022-08-17 19:25:23 -0700

    Time: 18 ms (14.29%), Space: 59.4 MB (11.51%) - LeetHub

--- 221-maximal-square/221-maximal-square.java
+++ 221-maximal-square/221-maximal-square.java
@@ -16,14 +16,6 @@ class Solution {
                 }
             }
         }
-       
-        for(int i=0;i<m+1;i++) {
-            for(int j=0;j<n+1;j++) {
-                System.out.print(dp[i][j]);
-            }
-             System.out.println();
-        }
-        
         return max*max;
     }
 }
