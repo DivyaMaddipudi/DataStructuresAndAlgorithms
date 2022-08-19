b7731e7e576cf73dc83589c3165df6edb2f95d9d
DivyaMaddipudi <30898620+DivyaMaddipudi@users.noreply.github.com>
2022-08-18 17:31:00 -0700

    Time: 2 ms (60.75%), Space: 46.8 MB (81.44%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.java
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.java
@@ -23,29 +23,39 @@ class Node {
 
 class Solution {
     public Node connect(Node root) {
-        
         if(root == null || root.left == null || root.right == null) return root;
         
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(root.left);
-        queue.add(root.right);
         
-
-        while(!queue.isEmpty()) {
-            int size = queue.size();
-            Node curr = queue.poll();
-            if(curr.left != null) queue.add(curr.left);
-            if(curr.right != null) queue.add(curr.right);
-            for(int i=1;i<size;i++) {
-                Node next = queue.poll();
-                if(next.left != null) queue.add(next.left);
-                if(next.right != null) queue.add(next.right);
-                curr.next = next; 
-                if(next != null) {
-                    curr = curr.next;
-                }
-            }
-        }
-        return root;        
+        dfs(root.left, root.right);
+        // Queue<Node> queue = new LinkedList<>();
+        // queue.add(root.left);
+        // queue.add(root.right);
+        // while(!queue.isEmpty()) {
+        //     int size = queue.size();
+        //     Node curr = queue.poll();
+        //     if(curr.left != null) queue.add(curr.left);
+        //     if(curr.right != null) queue.add(curr.right);
+        //     for(int i=1;i<size;i++) {
+        //         Node next = queue.poll();
+        //         if(next.left != null) queue.add(next.left);
+        //         if(next.right != null) queue.add(next.right);
+        //         curr.next = next; 
+        //         if(next != null) {
+        //             curr = curr.next;
+        //         }
+        //     }
+        // }
+        // return root; 
+        return root;
+    }
+    
+    private void dfs(Node left, Node right) {
+        // base
+        if(left == null) return;
+        // logic
+        left.next = right;
+        dfs(left.left, left.right);
+        dfs(left.right, right.left);
+        dfs(right.left, right.right);
     }
 }
