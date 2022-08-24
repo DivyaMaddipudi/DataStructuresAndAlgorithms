c3f3d1d833ceb5dbe0446bc91c8593dca6488c0b
DivyaMaddipudi <30898620+DivyaMaddipudi@users.noreply.github.com>
2022-08-24 09:28:11 -0700

    Time: 1 ms (87.15%), Space: 64.3 MB (64.49%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -1,14 +1,12 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int[] copy = new int[nums.length];
-        for(int i=0;i<nums.length;i++) {
-            copy[(i+k)%nums.length] = nums[i];
-        }
-      
-        for(int i=0;i<copy.length;i++) {
-            nums[i] = copy[i];
-        }
         
+        int n = nums.length;
+        k = k % n;
+        
+        reverse(nums, n-k, n-1);
+        reverse(nums, 0, n-k-1);
+        reverse(nums, 0, n-1);
         //BRUTE FORCE
         // while(k>0) {
         // int temp = nums[nums.length-1];
@@ -18,5 +16,20 @@ class Solution {
         // nums[0] = temp;
         // k--;
         // }
+    }
+    
+    private void reverse(int[] nums, int left, int right) {
+        
+        while(left <= right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+    
+    private void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
     }
 }
